81899602b5c450c351a8e37fa09460b3e005d28a
Yury Klushin <wicked_digger@mail.ru>
2015-02-23 19:44:06 +0300

    Fixed: merge conflict

--- src/gui.cc
+++ src/gui.cc
@@ -58,20 +58,6 @@ gui_object_t::~gui_object_t()
   delete_frame();
 }
 
-gui_object_t::~gui_object_t()
-{
-  delete_frame();
-}
-
-void
-gui_object_t::delete_frame()
-{
-  if(frame != NULL) {
-    gfx_frame_destroy(frame);
-    frame = NULL;
-  }
-}
-
 void
 gui_object_t::delete_frame()
 {
